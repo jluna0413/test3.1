a8b0be396111177ca965b3334e7392a3b9cd7818
Prelang Builder <builder@prelang.com>
2015-10-19 20:53:21 +0000

    Generated model Job with migration

---
+++ spec/factories/jobs.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :job do
+    client nil
+description "MyText"
+jobstart "2015-10-19 20:53:21"
+jobend "2015-10-19 20:53:21"
+talenttype "MyString"
+payrate "9.99"
+jobzip "MyString"
+jobaddress "MyText"
+  end
+
+end
